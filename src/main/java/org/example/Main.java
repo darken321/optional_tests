f06df5ec34ec06219ea6aeedc2ecdaacbd32e3a3
Vasili Bandarenka <darken123@inbox.ru>
2023-08-25 13:57:39 +0300

    Option class tests samples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,29 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Optional<String> nothing1 = Optional.empty();
+        Optional<String> nothing = Optional.ofNullable(null);
+        System.out.println("Optional.empty() = Optional.ofNullable(null)  " + (nothing1 == nothing));
+        Optional<String> anything = Optional.ofNullable("test text");
+
+        System.out.println(nothing);
+        System.out.println("Объект есть " + nothing.isPresent());
+        System.out.println("Объект пуст " + nothing.isEmpty());
+        nothing.ifPresent(s -> System.out.println(s + " есть")); // не выведется
+
+        System.out.println();
+        System.out.println(anything);
+        System.out.println("Объект есть " + anything.isPresent());
+        System.out.println("Объект пуст " + anything.isEmpty());
+        anything.ifPresent(s -> System.out.println(s + " есть")); //выведется
+        String s1 = anything.orElseThrow();
+        String s2 = nothing.orElseThrow();// выбрасывается NoSuchElementException
+
+
     }
 }
